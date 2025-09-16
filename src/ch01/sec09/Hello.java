1215c13a5aac3629d52d7bb613639cf8399a04d6
Kim Minseok <rlaalstjr0806@gmail.com>
2025-09-16 14:11:08 +0900

    자바 예제문항 업로드, ch = 챕터, sec = 섹션

---
+++ src/ch01/sec09/Hello.java
@@ -0,0 +1,14 @@
+package ch01.sec09;
+
+public class Hello { // about security, public
+
+	public static void main(String[] args) { // void : return nothing, String[] args : array of strings
+		int x;
+		x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+
+	}
+
+}
---
+++ src/ch02/sec01/VarTest.java
@@ -0,0 +1,10 @@
+package ch02.sec01;
+
+public class VarTest {
+    public static void main(String[] args) {
+        int value = 10; // declaration
+        int result = value + 10;
+        System.out.print(result); // 값 할당
+        System.out.println(" <-");
+    }
+}
